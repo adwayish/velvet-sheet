b4ac29612053f5bc7baadd292806ee30bd66c7cd
adwayish <adwayish@gmail.com>
2013-01-23 23:19:57 +1100

    initial commit

---
+++ Experiment.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+# <nbformat>3.0</nbformat>
+
+# <codecell>
+
+import pandas as pd
+
+# <markdowncell>
+
+# #Velvet Experiment Sheet
+
+# <codecell>
+
+"""
+set up experiment parameters
+k : kmer length
+cvCut : kmer coverage
+
+return to vlevlet manual for more about them. 
+"""
+
+conf = {}
+conf['k'] = [15,21,31]
+conf['cvCut'] = [8,10]
+conf['expCov']= map(lambda(x): x*2,conf['cvCut'])
+conf['output']= "output"
+conf['seq'] = "EClen60cov20.fna"
+
+#### Utils
+def mkname(k,cvCut): return '_'.join([conf['output'],str(k),str(cvCut)])
+conf['ls']= [mkname(k,cv) for k in conf['k'] for cv in conf['cvCut']]
+
+# <codecell>
+
+!pwd
+!ls
+for f in conf['ls']:
+    !rm -rf $f
+
+# <codecell>
+
+"""
+Run the main rotuin to generate data files
+this include runing velvet binaries (vleveth and vlevetg) 
+for conf[k] x conf[cvCut] times.
+"""
+for k in conf['k']: 
+    !echo {conf['output']} {str(k)} -short {conf['seq']}
+    !velveth {conf['output']} {str(k)} -short {conf['seq']};
+    for cvCut in conf['cvCut']:
+        f = mkname(k,cvCut)
+        !cp -R {conf['output']} {f}
+        !velvetg {f} -cov_cutoff {str(cvCut)};
+    !rm -rf {conf['output']}
+
+# <codecell>
+
+"""
+Collect Data files into dataframe with the following columns
+k cvCut n50 totalLgth contigs_ls_2 mean max min
+"""
+def collect(f):
+    stats = pd.read_table('/'.join([f,"stats.txt"]))
+    minLgth = stats['lgth'].min()
+    maxLgth = stats['lgth'].max()
+    meanLgth= stats['lgth'].mean()
+    medianLgth=stats['lgth'].median()
+    
+for f in conf['ls']:
+    plt.figure()
+   
+    stats[stats['lgth'] > 5000][['lgth','short1_cov']].hist()
+
---
+++ README.md
@@ -0,0 +1,26 @@
+#Velvet Experiment Sheet
+
+[Velvet][1] is de novo assembler for NGS reads.
+Influnced by [standardized-velvet-assembly-report][2], 
+velvet experiment sheet is a single [ipython notebook][3] that can 
+help you run multiple assemblies with differe parameters, and visualize the result. 
+
+<!-- You can use this experiment sheet to keep track of your tasks, and your results in the same place.
+using ipython notebook this is possible. Also, an advantage of using is that you can put your experiment sheet
+under source control and track the history of your experiment. -->
+
+## Requirement
+1. [ipython notebook][3]
+2. [pandas][4]
+3. [matplotlib][5]
+
+TODO:
+1. Generate data for different parameters.
+2. collect data for different expriment. 
+3. Visualize and plot.
+
+[1]:[https://github.com/dzerbino/velvet]
+[2]:[http://code.google.com/p/standardized-velvet-assembly-report/]
+[3]:[http://ipython.org/ipython-doc/dev/interactive/htmlnotebook.html]
+[4]:[http://pandas.pydata.org]
+[5]:[http://matplotlib.org]
