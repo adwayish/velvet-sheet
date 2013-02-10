5d886cd86b503fec4cbb3cae6802e9fc06c8914a
adwayish <adwayish@gmail.com>
2013-02-10 15:37:16 +1100

    Exploring more ideas on how to collect settings from user, and run the experiment.
    - Renamed Config to Experiment.
    - added iteration list to Experiment, making the Experiment iterate-able. This is nice
    but having a separate list for settings (kmers,cvCuts) allow more optimized main loop when
    generating data with velveth.

--- Experiment.py
+++ Experiment.py
@@ -4,6 +4,7 @@
 # <codecell>
 
 import pandas as pd
+from collections import namedtuple
 
 # <markdowncell>
 
@@ -11,29 +12,32 @@ import pandas as pd
 
 # <codecell>
 
-"""
-set up experiment parameters
-k : kmer length
-cvCut : kmer coverage
+###
+# set up experiment parameters
+# kmers : kmer length
+# cvCut : coverage threshold per contig 
+#
+# refer to vlevlet manual for more details. 
+###
 
-return to vlevlet manual for more about them. 
-"""
+class Experiment: pass
+ex = Experiment()
+ex.kmers = [1,2,3,4] 
+ex.cvCut  = [8,10]   
+ex.expCov = map(lambda(x): x*2,ex.cvCut)
+ex.output = "output"
+ex.seq = "EClen60cov20.fna"
 
-conf = {}
-conf['k'] = [15,21,31]
-conf['cvCut'] = [8,10]
-conf['expCov']= map(lambda(x): x*2,conf['cvCut'])
-conf['output']= "output"
-conf['seq'] = "EClen60cov20.fna"
+def mkname(k,cvCut): return '_'.join([ex.output,str(k),str(cvCut)]) 
 
-#### Utils
-def mkname(k,cvCut): return '_'.join([conf['output'],str(k),str(cvCut)])
-conf['ls']= [mkname(k,cv) for k in conf['k'] for cv in conf['cvCut']]
+# build the iteration list
+Iteration = namedtuple('Iteration','kmer cvCut dirname')
+ex.iterations =[Iteration(kmer,cvCut,mkname(kmer,cvCut)) for kmer in ex.kmers for cvCut in ex.cvCut]
 
 # <codecell>
 
 # clean
-for f in conf['ls']:
+for f in ex.dirs:
     !rm -rf {f}
 
 # <codecell>
@@ -43,32 +47,30 @@ Run the main rotuin to generate data files
 this include runing velvet binaries (vleveth and vlevetg) 
 for conf[k] x conf[cvCut] times.
 """
-for k in conf['k']: 
-    !echo {conf['output']} {str(k)} -short {conf['seq']}
-    !velveth {conf['output']} {str(k)} -short {conf['seq']};
-    for cvCut in conf['cvCut']:
+for k in ex.kmers: 
+    !velveth {ex.output} {str(k)} -short {ex.seq};
+    for cvCut in ex.cvCut:
         f = mkname(k,cvCut)
-        !cp -R {conf['output']} {f}
+        !cp -R {ex.output} {f}
         !velvetg {f} -cov_cutoff {str(cvCut)};
-    !rm -rf {conf['output']}
+    !rm -rf {ex.output}
 
 # <codecell>
 
 """
-Collect Data files into dataframe with the following columns
+Collect data from files into dataframe has the following columns
 k cvCut n50 totalLgth contigs_ls_2 mean max min
 """
-def collect(f):
+def collect(f,kmer):
     """
     recieve a file path, returns a row of the required information.
     """
     stats = pd.read_table('/'.join([f,"stats.txt"]))
-    sumLgth = (stats['lgth'] + (conf['k']-1)).sum()
-    minLgth = (stats['lgth'] + (conf['k']-1)).min()
-    maxLgth = (stats['lgth'] + (conf['k']-1)).max()
-    meanLgth= (stats['lgth'] + (conf['k']-1)).mean()
-    medianLgth=(stats['lgth'] + (conf['k']-1)).median()
-    [sumLgth,minLgth,maxLgth,meanLgth,medianLgth]
-    
-    
+    stats['lgth'] = (stats['lgth'] + (kmer-1))
+    sumLgth = stats['lgth'].sum()
+    minLgth = stats['lgth'].min()
+    maxLgth = stats['lgth'].max()
+    meanLgth= stats['lgth'].mean()
+    medianLgth=stats['lgth'].median()
+    [sumLgth,minLgth,maxLgth,meanLgth,medianLgth] 
 
